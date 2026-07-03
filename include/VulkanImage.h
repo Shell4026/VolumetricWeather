bbfd9949470dab3ba973e6a7090c0d8cc1cd3ba7
Shell <104874910+Shell4026@users.noreply.github.com>
2026-07-03 18:02:39 +0900

    feat: VulkanImage클래스 구현

---
+++ include/VulkanImage.h
@@ -0,0 +1,60 @@
+﻿#pragma once
+#include "VulkanContext.h"
+
+class VulkanSampler
+{
+public:
+	VulkanSampler(const VulkanSampler& other) = delete;
+	VulkanSampler(VulkanSampler&& other) noexcept :
+		device(other.device),
+		sampler(other.sampler),
+		info(other.info)
+	{
+		other.sampler = VK_NULL_HANDLE;
+	}
+	~VulkanSampler();
+
+	auto GetSampler() const -> VkSampler { return sampler; }
+	auto GetInfo() const -> const VkSamplerCreateInfo& { return info; }
+
+	static auto Create(const VulkanContext& ctx, const VkSamplerCreateInfo& ci) -> VulkanSampler;
+protected:
+	VulkanSampler() = default;
+private:
+	VkDevice device = VK_NULL_HANDLE;
+	VkSampler sampler = VK_NULL_HANDLE;
+	VkSamplerCreateInfo info{};
+};
+
+class VulkanImage
+{
+public:
+	VulkanImage(const VulkanImage& other) = delete;
+	VulkanImage(VulkanImage&& other) noexcept :
+		device(other.device),
+		img(other.img),
+		view(other.view),
+		mem(other.mem),
+		info(other.info)
+	{
+		other.img = VK_NULL_HANDLE;
+		other.view = VK_NULL_HANDLE;
+		other.mem = VK_NULL_HANDLE;
+	}
+	~VulkanImage();
+
+	auto GetImage() const -> VkImage { return img; }
+	auto GetView() const -> VkImageView { return view; }
+	auto GetMemory() const -> VkDeviceMemory { return mem; }
+	auto GetInfo() const -> const VkImageCreateInfo& { return info; }
+
+	static auto Create(const VulkanContext& ctx, const VkImageCreateInfo& ci, VkImageAspectFlags aspect, VkMemoryPropertyFlags memProp) -> VulkanImage;
+protected:
+	VulkanImage() = default;
+private:
+	VkDevice device = VK_NULL_HANDLE;
+	VkImage img = VK_NULL_HANDLE;
+	VkImageView view = VK_NULL_HANDLE;
+	VkDeviceMemory mem = VK_NULL_HANDLE;
+	VkImageCreateInfo info{};
+};
---
+++ src/VulkanImage.cpp
@@ -0,0 +1,54 @@
+﻿#include "VulkanImage.h"
+#include "Logger.h"
+
+auto VulkanSampler::Create(const VulkanContext& ctx, const VkSamplerCreateInfo& ci) -> VulkanSampler
+{
+	VulkanSampler sampler{};
+	sampler.device = ctx.GetDevice();
+	sampler.info = ci;
+	VK_RESULT_CHECK(vkCreateSampler(sampler.device, &ci, nullptr, &sampler.sampler));
+	return sampler;
+}
+
+VulkanSampler::~VulkanSampler()
+{
+	if (sampler == VK_NULL_HANDLE)
+		return;
+	vkDestroySampler(device, sampler, nullptr);
+}
+
+auto VulkanImage::Create(const VulkanContext& ctx, const VkImageCreateInfo& ci, VkImageAspectFlags aspect, VkMemoryPropertyFlags memProp) -> VulkanImage
+{
+	VulkanImage img{};
+	img.device = ctx.GetDevice();
+	img.info = ci;
+	VK_RESULT_CHECK(vkCreateImage(img.device, &ci, nullptr, &img.img));
+
+	VkMemoryRequirements memReqs;
+	vkGetImageMemoryRequirements(img.device, img.img, &memReqs);
+	VkMemoryAllocateInfo memAllocInfo{};
+	memAllocInfo.sType = VkStructureType::VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+	memAllocInfo.allocationSize = memReqs.size;
+	memAllocInfo.memoryTypeIndex = ctx.FindMemoryType(memReqs.memoryTypeBits, VkMemoryPropertyFlagBits::VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+	VK_RESULT_CHECK(vkAllocateMemory(img.device, &memAllocInfo, nullptr, &img.mem));
+	VK_RESULT_CHECK(vkBindImageMemory(img.device, img.img, img.mem, 0));
+
+	VkImageViewCreateInfo viewCi{};
+	viewCi.sType = VkStructureType::VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+	viewCi.viewType = VkImageViewType::VK_IMAGE_VIEW_TYPE_2D;
+	viewCi.format = img.info.format;
+	viewCi.subresourceRange = { aspect, 0, 1, 0, 1 };
+	viewCi.image = img.img;
+	VK_RESULT_CHECK(vkCreateImageView(img.device, &viewCi, nullptr, &img.view));
+
+	return img;
+}
+
+VulkanImage::~VulkanImage()
+{
+	if (img == VK_NULL_HANDLE)
+		return;
+	vkDestroyImageView(device, view, nullptr);
+	vkFreeMemory(device, mem, nullptr);
+	vkDestroyImage(device, img, nullptr);
+}
