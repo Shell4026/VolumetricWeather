8d4849003203a9a4394544cdba6ecfef9b5d2b94
Shell <104874910+Shell4026@users.noreply.github.com>
2026-08-13 00:28:03 +0900

    feat: 아티스트 파라미터 레이어 뼈대

---
+++ include/weather/ArtistCloudSetting.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+struct ArtistCloudSetting
+{
+	float amount = 0.35f;
+	float size = 0.5f;
+	float detail = 0.5f;
+	float density = 0.4f;
+	float verticalDevelopment = 0.4f;
+
+	float darkness = 0.2f;
+	float softness = 0.5f;
+
+	float windDirectionDegrees = 0.0f;
+	float windSpeedKmh = 10.0f;
+};
---
+++ include/weather/CloudParameterMapper.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "weather/ArtistCloudSetting.h"
+
+#include "pass/CloudPass.h"
+
+class CloudParameterMapper
+{
+public:
+	static auto ConvertRenderSetting(const ArtistCloudSetting& artistSetting, const CloudPass::Setting& baseSetting) -> CloudPass::Setting;
+};
