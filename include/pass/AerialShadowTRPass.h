9a2e2d736ae384022b9d1bc1effa03e0a965c06e
Shell <104874910+Shell4026@users.noreply.github.com>
2026-08-19 12:04:36 +0900

    temp

---
+++ include/pass/AerialShadowTRPass.h
@@ -0,0 +1,64 @@
+﻿#pragma once
+#include "pass/APass.h"
+
+#include "glm/glm.hpp"
+
+#include <memory>
+
+class Shader;
+class Material;
+class LUTPass;
+
+class AerialShadowTRPass : public APass
+{
+public:
+	struct alignas(16) Setting
+	{
+		glm::vec3 pos{ 0.f };
+		alignas(16) glm::mat4 viewProj{ 1.f };
+
+		float historyWeight = 0.95f;
+		uint32_t historyValid = 0;
+	};
+public:
+	AerialShadowTRPass(const LUTPass& cloudPass);
+
+	void Clear() override;
+
+	void SetUsages(const FrameContext& frame) override;
+
+	void BeginRecord(const FrameContext& frame, const std::vector<BarrierInfo>* barrierInfos = nullptr) override;
+	void Record(const FrameContext& frame) override;
+	void EndRecord(const FrameContext& frame, const std::vector<BarrierInfo>* barrierInfos = nullptr) override;
+
+	void SetSetting(const Setting& setting);
+	void InvalidateHistory();
+	void SetHistoryValid(uint32_t valid);
+
+	auto GetOutputImage() const -> const VulkanImage* { return curOutput; }
+	auto GetSampler() const -> const VulkanSampler* { return sampler; }
+	auto GetSetting() const -> const Setting& { return setting; }
+protected:
+	void PrepareResource(const VulkanContext& ctx, VkDescriptorSetLayout cameraSetLayout) override;
+	void SetupDescriptors(const VulkanContext& ctx, VkDescriptorPool descPool) override;
+	void BuildPipeline(const VulkanContext& ctx) override;
+private:
+	void CreateTRShader(VkDescriptorSetLayout cameraSetLayout);
+private:
+	const LUTPass& cloudPass;
+
+	std::unique_ptr<VulkanImage> output;
+	std::unique_ptr<VulkanImage> output2;
+	std::unique_ptr<VulkanImage> accum;
+	std::unique_ptr<Shader> shader;
+	std::unique_ptr<Material> material;
+	VkPipeline pipeline = VK_NULL_HANDLE;
+
+	const VulkanImage* curOutput = nullptr;
+	const VulkanImage* prevOutput = nullptr;
+	const VulkanSampler* sampler = nullptr;
+
+	Setting setting;
+	uint32_t frameIdx = 0;
+	uint64_t settingRevision = 0;
+};
